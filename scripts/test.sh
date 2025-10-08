8bb96505699684965c96bff4d1c30176daf275d2
Chonghua Liao <lch22@mails.tsinghua.edu.cn>
2025-10-08 15:45:22 +0800

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,5 @@
-cd ./eval
 export CUDA_VISIBLE_DEVICES="0"
 PROMPT_TYPE="shepherd" # qwen25-math-cot / chatml
 MODEL_NAME_OR_PATH=$1
 OUTPUT_DIR=${2:-"./results"}
-bash sh/eval.sh $PROMPT_TYPE $MODEL_NAME_OR_PATH $OUTPUT_DIR
+bash ./eval/sh/eval.sh $PROMPT_TYPE $MODEL_NAME_OR_PATH $OUTPUT_DIR
